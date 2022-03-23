4834c082514917bc49b61c6784362608862e40d7
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-03-23 21:38:08 +0200

    Methods - assignments

---
+++ Methods/PalindromeIntegers/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            while ((number = Console.ReadLine()) != "END")
+            {
+                if (IsNumberPalindrome(number))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+
+        }
+
+        static bool IsNumberPalindrome(string number)
+        {
+            bool isPalindrome = false;
+            if (number.Length == 1)
+            {
+                isPalindrome = true;
+            }
+
+            for (int i = 0; i < number.Length / 2; i++)
+            {
+                for (int j = number.Length - 1 - i; j > number.Length / 2; j--)
+                {
+                    if (number[i] == number[j])
+                    {
+                        isPalindrome = true;
+                        break;
+                    }
+                    else
+                    {
+                        isPalindrome = false;
+                        break;
+                    }
+
+                }
+                if (!isPalindrome)
+                {
+                    break;
+                }
+
+            }
+            return isPalindrome;
+        }
+    }
+}
---
+++ Methods/TopNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TopNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            isTopNumber(number);
+        }
+
+        static void isTopNumber(int number)
+        {
+            int sumOfDigits = 0;
+            bool containsOddValue = false;
+            for (int i = 17; i <= number; i++)
+            {
+                containsOddValue = false;
+                sumOfDigits = 0;
+                int tempNumb = i;
+                while (tempNumb > 0)
+                {
+                    int lastDigit = tempNumb % 10;
+                    tempNumb /= 10;
+
+                    if (lastDigit % 2 != 0)
+                    {
+                        containsOddValue = true;
+                    }
+
+                    sumOfDigits += lastDigit;
+                }
+
+                if (sumOfDigits % 8 == 0 && containsOddValue == true)
+                {
+                    Console.WriteLine($"{i}");
+                }
+
+            }
+        }
+    }
+}
