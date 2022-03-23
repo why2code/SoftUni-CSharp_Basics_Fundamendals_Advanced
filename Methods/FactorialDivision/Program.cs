a25b59e9a1ae8d710bea82cc91d0e7e1c704d251
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-03-23 21:35:54 +0200

    Methods - assignments

---
+++ Methods/FactorialDivision/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Numerics;
+
+namespace FactorielDevision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{FactorielDevision(firstNumber, secondNumber):F2}");
+        }
+
+        static double FactorielDevision(int a, int b)
+        {
+            double factorielOne = 1;
+            double factorielTwo = 1;
+
+            if (a == 0)
+            {
+                factorielOne = 1;
+            }
+            else
+            {
+                for (int i = 1; i <= a; i++)
+                {
+                    factorielOne *= (double)i;
+                }
+            }
+
+            if (b == 0)
+            {
+                factorielTwo = 1;
+            }
+            else
+            {
+                for (int j = 1; j <= b; j++)
+                {
+                    factorielTwo *= (double)j;
+                }
+            }
+
+            return factorielOne / factorielTwo;
+        }
+    }
+}
---
+++ Methods/NxNMatrix/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NxNMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixNumber = int.Parse(Console.ReadLine());
+            PrintMatrix(matrixNumber);
+        }
+
+        static void PrintMatrix(int matrixNumber)
+        {
+            for (int i = 0; i < matrixNumber; i++)
+            {
+                for (int j = 0; j < matrixNumber; j++)
+                {
+                    if (j == matrixNumber - 1)
+                    {
+                        Console.Write($"{matrixNumber}");
+
+                    }
+                    else
+                    {
+                        Console.Write($"{matrixNumber} ");
+
+                    }
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
