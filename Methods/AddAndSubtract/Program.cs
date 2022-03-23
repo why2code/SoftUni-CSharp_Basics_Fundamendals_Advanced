06e547710a8629e2041582cb34838dec3de4bfac
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-03-23 21:31:19 +0200

    Methods - assignments

---
+++ Methods/AddAndSubtract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int result = SubtractNumbers(SumNumbers(firstNumber, secondNumber), thirdNumber);
+            Console.WriteLine(result);
+        }
+
+
+
+        static int SumNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int SubtractNumbers(int a, int c)
+        {
+            return a - c;
+        }
+    }
+}
---
+++ Methods/MiddleCharacters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(MiddleCharacters(input));
+
+        }
+
+        static string MiddleCharacters(string text)
+        {
+            int textLenght = text.Length;
+            string output = string.Empty;
+            if (textLenght % 2 == 0)
+            {
+                for (int i = (textLenght / 2) - 1; i <= (textLenght / 2); i++)
+                {
+                    output += text[i];
+                }
+            }
+            else
+            {
+                output += text[textLenght / 2];
+            }
+            return output;
+        }
+    }
+}
