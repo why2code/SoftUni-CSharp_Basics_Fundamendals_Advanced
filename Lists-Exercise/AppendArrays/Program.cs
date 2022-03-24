21d409538784d355432dd34f1f6a0647b6d9ab81
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-03-24 19:31:55 +0200

    Lists exercise

---
+++ Lists-Exercise/AppendArrays/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppendArrays
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> initialList = Console.ReadLine().Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> resultList = new List<string>();
+
+            ListSwap(initialList);
+            foreach (var item in initialList)
+            {
+                resultList.AddRange(item.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList());
+            }
+                        
+
+            Console.WriteLine(string.Join(" ", resultList));
+
+        }
+
+        static void ListSwap(List<string> list)
+        {
+
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                string firstSwap = list[list.Count - 1];
+                list.RemoveAt(list.Count - 1);
+                list.Insert(list.Count - i, firstSwap);
+            }
+        }
+
+        public static string RemoveWhitespace(string input)
+        {
+
+            input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            return input;
+        }
+
+
+    }
+}
---
+++ Lists-Exercise/CardsGame/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CardsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> deckOne = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> deckTwo = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            while (deckOne.Count > 0 && deckTwo.Count > 0)
+            {
+                CompareCardsInDeck(deckOne, deckTwo);
+            }
+
+            if (deckOne.Count > 0)
+            {
+                Console.WriteLine($"First player wins! Sum: {deckOne.Sum()}");
+            }
+            else if (deckTwo.Count > 0)
+            {
+                Console.WriteLine($"Second player wins! Sum: {deckTwo.Sum()}");
+            }
+        }
+
+        static void CompareCardsInDeck(List<int> firstList, List<int> secondList)
+        {
+            int cardOne = firstList[0];
+            int cardTwo = secondList[0];
+
+            if (cardOne > cardTwo)
+            {
+                int winningCard = firstList[0];
+                firstList.RemoveAt(0);
+                firstList.Add(winningCard);
+                firstList.Add(cardTwo);
+                secondList.RemoveAt(0);
+            }
+            else if (cardTwo > cardOne)
+            {
+                int winningCard = secondList[0];
+                secondList.RemoveAt(0);
+                secondList.Add(winningCard);
+                secondList.Add(cardOne);
+                firstList.RemoveAt(0);
+            }
+            else if (cardOne == cardTwo)
+            {
+                firstList.RemoveAt(0);
+                secondList.RemoveAt(0);
+            }
+        }
+    }
+}
