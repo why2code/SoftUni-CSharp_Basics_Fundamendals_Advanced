7c98f37c65fdf69a116a0b4e3d910707fd7b7423
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-03-24 19:37:53 +0200

    Objects and Classes - assignments

---
+++ ObjectsAndClasses/AdvertisementMessage/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdvertismentMessage
+{
+    class Advertisment
+    {
+        public Advertisment()
+        {
+            this.phrases = new List<string>()
+            {
+                "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product."
+            };
+
+            this.events = new List<string>()
+            {
+                "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!"
+            };
+
+            this.authors = new List<string>()
+            {
+                "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"
+            };
+
+            this.cities = new List<string>()
+            {
+                "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"
+            };
+        }
+
+
+        public List<string> phrases { get; set; }
+        public List<string> events { get; set; }
+        public List<string> authors { get; set; }
+        public List<string> cities { get; set; }
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int counter = int.Parse(Console.ReadLine());
+            for (int i = 0; i < counter; i++)
+            {
+                Advertisment add = new Advertisment();
+                List<string> mixedAdvertisment = new List<string>();
+
+                string phraseRandom = add.phrases[rand.Next(0, add.phrases.Count)];
+                string eventRandom = add.events[rand.Next(0, add.events.Count)];
+                string authorRandom = add.authors[rand.Next(0, add.authors.Count)];
+                string cityRandom = add.cities[rand.Next(0, add.cities.Count)];
+
+                mixedAdvertisment.Add(phraseRandom);
+                mixedAdvertisment.Add(" ");
+                mixedAdvertisment.Add(eventRandom);
+                mixedAdvertisment.Add(" ");
+                mixedAdvertisment.Add(authorRandom);
+                mixedAdvertisment.Add(" – ");
+                mixedAdvertisment.Add(cityRandom);
+                mixedAdvertisment.Add(".");
+
+                Console.WriteLine(String.Join("", mixedAdvertisment));
+
+            }
+        }
+    }
+}
---
+++ ObjectsAndClasses/Articles/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Articles
+{
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            this.Title = title;
+            this.Content = content;
+            this.Author = author;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void EditContent(string content)
+        {
+            Content = content;
+        }
+        public void ChangeAutroh(string author)
+        {
+            Author = author;
+        }
+        public void Rename(string newTitle)
+        {
+            Title = newTitle;
+        }
+
+        public override string ToString()
+        {
+            return ($"{this.Title} - {this.Content}: {this.Author}");
+        }
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] articleInitialInput = Console.ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries);
+            string articleTitle = articleInitialInput[0].Trim();
+            string articleContent = articleInitialInput[1].Trim();
+            string articleAuthor = articleInitialInput[2].Trim();
+
+            Article newArticleCreated = new Article(articleTitle, articleContent, articleAuthor);
+
+            int commantsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < commantsCount; i++)
+            {
+                List<string> receivedCommands = Console.ReadLine().Split(':', StringSplitOptions.RemoveEmptyEntries).ToList();
+                string command = receivedCommands[0];
+                string textToEdit = receivedCommands[1].Trim();
+
+                if (command == "Edit")
+                {
+                    newArticleCreated.EditContent(textToEdit);
+                }
+                else if (command == "ChangeAuthor")
+                {
+                    newArticleCreated.ChangeAutroh(textToEdit);
+                }
+                else if (command == "Rename")
+                {
+                    newArticleCreated.Rename(textToEdit);
+                }
+            }
+
+            Console.WriteLine(newArticleCreated.ToString());
+        }
+    }
+}
