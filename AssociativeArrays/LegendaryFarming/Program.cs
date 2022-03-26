2e1cd88fce5ada11755dcb0d5aed628f8332da9d
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-03-26 13:15:29 +0200

    AssociativeArrays - exercise

---
+++ AssociativeArrays/LegendaryFarming/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._0_LegendaryFarming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> specialItems = new Dictionary<string, double>()
+            {
+                ["shards"] = 0,
+                ["motes"] = 0,
+                ["fragments"] = 0
+            };
+            Dictionary<string, double> junkItems = new Dictionary<string, double>();
+            string specialItemID = null;
+
+            while (specialItemID == null)
+            {
+                string[] currentItemsFound = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < currentItemsFound.Length - 1; i += 2)
+                {
+                    double value = double.Parse(currentItemsFound[i]);
+                    string typeOfItem = currentItemsFound[i + 1].ToLower();
+
+                    if (typeOfItem == "shards" || typeOfItem == "fragments" || typeOfItem == "motes")
+                    {
+                        if (specialItems.ContainsKey(typeOfItem))
+                        {
+                            specialItems[typeOfItem] += value;
+                        }
+                        else
+                        {
+                            specialItems.Add(typeOfItem, value);
+                        }
+
+                        //checking for completed item (250 or more pieces gathered) to break the cycle
+                        foreach (var item in specialItems)
+                        {
+                            if (item.Value >= 250)
+                            {
+                                string specialItem = item.Key;
+                                if (specialItem == "shards")
+                                {
+                                    Console.WriteLine("Shadowmourne obtained!");
+                                    specialItemID = "shards";
+                                    break;
+                                }
+                                else if (specialItem == "fragments")
+                                {
+                                    Console.WriteLine("Valanyr obtained!");
+                                    specialItemID = "fragments";
+                                    break;
+                                }
+                                else if (specialItem == "motes")
+                                {
+                                    Console.WriteLine("Dragonwrath obtained!");
+                                    specialItemID = "motes";
+                                    break;
+                                }
+                            }
+                        }
+                        //if we have 250 or more of the special item, we break the cycle and reduce it`s value
+                        if (specialItemID != null)
+                        {
+                            specialItems[specialItemID] -= 250;
+                            break;
+                        }
+
+
+                    }
+                    else
+                    {
+                        if (!junkItems.ContainsKey(typeOfItem))
+                        {
+                            junkItems.Add(typeOfItem, value);
+                        }
+                        else
+                        {
+                            junkItems[typeOfItem] += value;
+                        }
+                    }
+                }
+
+
+            }
+
+            //final printout remaining items
+            foreach (var item in specialItems)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+            foreach (var item in junkItems)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
+        }
+    }
+}
---
+++ AssociativeArrays/Orders/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Orders
+{
+    public class Product
+    {
+        public Product(double price, double quantity)
+        {
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
+        public double TotalPrice { get; set; }
+        public double CalculateTotalPrice(double price, double quantity)
+        {
+            return this.TotalPrice = price * quantity;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Product> listOfProducts = new Dictionary<string, Product>();
+
+            string command = Console.ReadLine();
+            while (command != "buy")
+            {
+                string[] separatedCommants = command.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string nameOfProduct = separatedCommants[0];
+                double priceOfProduct = double.Parse(separatedCommants[1]);
+                double quantityOfProduct = double.Parse(separatedCommants[2]);
+
+                if (!listOfProducts.ContainsKey(nameOfProduct))
+                {
+                    Product prodToAdd = new Product(priceOfProduct, quantityOfProduct);
+                    prodToAdd.CalculateTotalPrice(priceOfProduct, quantityOfProduct);
+                    listOfProducts.Add(nameOfProduct, prodToAdd);
+                }
+                else
+                {
+                    listOfProducts[nameOfProduct].Price = priceOfProduct;
+                    listOfProducts[nameOfProduct].Quantity += quantityOfProduct;
+                    listOfProducts[nameOfProduct].TotalPrice =
+                        listOfProducts[nameOfProduct].CalculateTotalPrice(priceOfProduct, listOfProducts[nameOfProduct].Quantity);
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in listOfProducts)
+            {
+                double totalPriceOfCurrentItem = item.Value.TotalPrice;
+                Console.WriteLine($"{item.Key} -> {totalPriceOfCurrentItem:F2}");
+            }
+        }
+    }
+}
