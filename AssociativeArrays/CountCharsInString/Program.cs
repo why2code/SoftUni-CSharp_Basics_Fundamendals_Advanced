18e6c8dfbd10a69e8f17adebecc614e32b3efde0
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-03-26 13:13:57 +0200

    AssociativeArrays - exercise

---
+++ AssociativeArrays/CountCharsInString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountChairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charDerivedFromWords = Console.ReadLine().ToCharArray().Where(x => x != ' ').ToArray();
+
+            Dictionary<char, int> countedLetters = new Dictionary<char, int>();
+
+
+            foreach (char character in charDerivedFromWords)
+            {
+                if (!countedLetters.ContainsKey(character))
+                {
+                    countedLetters.Add(character, 1);
+                }
+                else
+                {
+                    countedLetters[character] += 1;
+                }
+            }
+
+            foreach (var item in countedLetters)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ AssociativeArrays/MinerTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace MinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, BigInteger> resourses = new Dictionary<string, BigInteger>();
+            string currentResource = Console.ReadLine();
+
+            while (currentResource != "stop")
+            {
+                BigInteger value = BigInteger.Parse(Console.ReadLine());
+
+                if (!resourses.ContainsKey(currentResource))
+                {
+                    resourses.Add(currentResource, value);
+                }
+                else
+                {
+                    resourses[currentResource] += value;
+                }
+
+                currentResource = Console.ReadLine();
+
+            }
+
+            foreach (var resource in resourses)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+        }
+    }
+}
