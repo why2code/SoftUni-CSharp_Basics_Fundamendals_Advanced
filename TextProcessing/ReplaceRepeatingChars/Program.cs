750dd6140843ed3c8bf6bca1031674476911c004
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-03-26 13:27:42 +0200

    Text Processing - exercise

---
+++ TextProcessing/ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == input[i + 1])
+                {
+                    input = input.Remove((i + 1), 1);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ TextProcessing/StringExplosion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace v._2._0_StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder output = new StringBuilder();
+            int bombPower = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currCharacter = input[i];
+                if (currCharacter == '>')
+                {
+                    output.Append(currCharacter);
+                    bombPower += (int)input[i + 1] - 48;
+                }
+                else
+                {
+                    if (bombPower > 0)
+                    {
+                        bombPower--;
+                    }
+                    else
+                    {
+                        output.Append(currCharacter);
+                    }
+                }
+
+            }
+
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
