3c166f714e15e73407111ba5f2fc2380724e9e55
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-03-26 13:23:00 +0200

    Text Processing - exercise

---
+++ TextProcessing/CharacterMultiplier/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int sumTotal = 0;
+
+            if (input[0].Length >= input[1].Length)
+            {
+                for (int i = 0; i < input[1].Length; i++)
+                {
+                    sumTotal += input[1][i] * input[0][i];
+                }
+
+                for (int i = input[1].Length; i < input[0].Length; i++)
+                {
+                    sumTotal += input[0][i];
+                }
+            }
+            else //input[0].Length < input[1].Length
+            {
+                for (int i = 0; i < input[0].Length; i++)
+                {
+                    sumTotal += input[0][i] * input[1][i];
+                }
+
+                for (int i = input[0].Length; i < input[1].Length; i++)
+                {
+                    sumTotal += input[1][i];
+                }
+            }
+
+            Console.WriteLine(sumTotal);
+        }
+
+    }
+}
---
+++ TextProcessing/ValidUsernames/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> usernames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            List<string> validUsers = new List<string>();
+
+            foreach (var user in usernames)
+            {
+                if (user.Length >= 3 && user.Length <= 16)
+                {
+                    if (user.All(Char.IsLetterOrDigit)
+                        || user.Contains('-') && user.All(Char.IsLetterOrDigit)
+                        || user.Contains('_') && user.All(Char.IsLetterOrDigit)
+                        || user.Contains('_') && user.Contains('-')
+                        || user.Contains('_')
+                        || user.Contains('-'))
+                    {
+                        validUsers.Add(user);
+
+                    }
+
+                    //if (user.All(c => Char.IsLetterOrDigit(c) || c == ' ' || c == '_' || c == '-'))
+                    //{
+                    //    validUsers.Add(user);
+                    //}
+
+                }
+            }
+
+            foreach (var user in validUsers)
+            {
+                Console.WriteLine($"{user}");
+            }
+        }
+    }
+}
