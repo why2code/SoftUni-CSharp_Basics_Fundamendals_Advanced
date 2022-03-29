b483e206ba5fec8eced1900d547359919c905840
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-03-29 19:15:27 +0300

    Regex_Exercise

---
+++ Regex_Exercise/ExtractEmails/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractEmails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?:[\s])\b(?<userName>[A-Za-z0-9]+[\.|\-|_]*[A-Za-z0-9]+\@{1})(?<host>[A-Za-z]+\-*[A-Za-z]*\.{1}[A-Za-z]+\.*[A-Za-z]*)\b";
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    Console.Write(match.Groups["userName"].Value);
+                    Console.Write(match.Groups["host"].Value);
+                    Console.WriteLine();
+                }
+
+            }
+
+        }
+    }
+}
