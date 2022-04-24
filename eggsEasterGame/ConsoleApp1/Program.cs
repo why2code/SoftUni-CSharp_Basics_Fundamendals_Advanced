a31b483163ba78803553af48713f9e33dd0a65f8
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-04-24 11:49:48 +0300

    Updates to the egg game

--- eggsEasterGame/ConsoleApp1/Program.cs
+++ eggsEasterGame/ConsoleApp1/Program.cs
@@ -6,7 +6,12 @@ namespace eggsEasterGame
     {
         static void Main(string[] args)
         {
-            int eggs = 12;
+            Console.WriteLine("Please enter the number of eggs hidden!");
+            Console.WriteLine("=======================================");
+            int eggs = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The game will now start, with {eggs} eggs total!");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("Please enter the name of the kid who found an egg, and how many eggs were found:");
 
             int kociEggs = 0;
             int sofiEggs = 0;
@@ -15,7 +20,7 @@ namespace eggsEasterGame
             string winner = null;
             int winnersEggs = 0;
 
-            while (totalEggsFound < 12)
+            while (totalEggsFound < eggs)
             {
                 string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
                 string kidName = input[0].ToLower();
@@ -24,21 +29,45 @@ namespace eggsEasterGame
                 // sofi 5
                 if (kidName == "sofi")
                 {
+                    if ((eggs - totalEggsFound) < eggsToAdd)
+                    {
+                        eggsToAdd = (eggs - totalEggsFound);
+                        Console.WriteLine($"{kidName} can not find more eggs than what is left hidden!");
+                        Console.WriteLine($"The number of eggs added to {kidName} will be {eggs - totalEggsFound}");
+                    }
                     sofiEggs += eggsToAdd;
                     totalEggsFound += eggsToAdd;
-                    Console.WriteLine($"{kidName.ToUpper()} just found {eggsToAdd} eggs!");
+                    Console.WriteLine($"{kidName.ToUpper()} just found {eggsToAdd} egg!");
+                    Console.WriteLine($"{kidName} has {sofiEggs} total eggs so far.");
+                    Console.WriteLine($"{eggs - totalEggsFound} eggs still remain hidden!");
                 }
                 else if (kidName == "ilko")
                 {
+                    if ((eggs - totalEggsFound) < eggsToAdd)
+                    {
+                        eggsToAdd = (eggs - totalEggsFound);
+                        Console.WriteLine($"{kidName} can not find more eggs than what is left hidden!");
+                        Console.WriteLine($"The number of eggs added to {kidName} will be {eggs - totalEggsFound}");
+                    }
                     ilkoEggs += eggsToAdd;
                     totalEggsFound += eggsToAdd;
-                    Console.WriteLine($"{kidName.ToUpper()} just found {eggsToAdd} eggs!");
+                    Console.WriteLine($"{kidName.ToUpper()} just found {eggsToAdd} egg!");
+                    Console.WriteLine($"{kidName} has {ilkoEggs} total eggs so far.");
+                    Console.WriteLine($"{eggs - totalEggsFound} eggs still remain hidden!");
                 }
                 else if (kidName == "koci")
                 {
+                    if ((eggs - totalEggsFound) < eggsToAdd)
+                    {
+                        eggsToAdd = (eggs - totalEggsFound);
+                        Console.WriteLine($"{kidName} can not find more eggs than what is left hidden!");
+                        Console.WriteLine($"The number of eggs added to {kidName} will be {eggs - totalEggsFound}");
+                    }
                     kociEggs += eggsToAdd;
                     totalEggsFound += eggsToAdd;
-                    Console.WriteLine($"{kidName.ToUpper()} just found {eggsToAdd} eggs!");
+                    Console.WriteLine($"{kidName.ToUpper()} just found {eggsToAdd} egg!");
+                    Console.WriteLine($"{kidName} has {kociEggs} total eggs so far.");
+                    Console.WriteLine($"{eggs - totalEggsFound} eggs still remain hidden!");
                 }
                 else
                 {
@@ -49,24 +78,53 @@ namespace eggsEasterGame
 
             }
 
+            // Checking who is the winner
+            bool winnerAvailable = false;
             if (ilkoEggs > kociEggs && ilkoEggs > sofiEggs)
             {
                 winner = "Ilko";
                 winnersEggs = ilkoEggs;
+                winnerAvailable = true;
             }
-            if (kociEggs > ilkoEggs && kociEggs > sofiEggs)
+            else if (kociEggs > ilkoEggs && kociEggs > sofiEggs)
             {
                 winner = "Koci";
                 winnersEggs = kociEggs;
+                winnerAvailable = true;
+
             }
-            if (sofiEggs > ilkoEggs && sofiEggs > kociEggs)
+            else if (sofiEggs > ilkoEggs && sofiEggs > kociEggs)
             {
                 winner = "Sofi";
                 winnersEggs = sofiEggs;
             }
+            else if ((ilkoEggs == kociEggs) && (sofiEggs == ilkoEggs) && (sofiEggs == kociEggs))
+            {
+                Console.WriteLine("Everyone has the same number of eggs!");
+                Console.WriteLine($"Everyone wins and has {eggs / 3} eggs each!");
+            }
+            else if (ilkoEggs == kociEggs)
+            {
+                Console.WriteLine($"Ilko and Koci have the same number of eggs, and both are winners with {(eggs - sofiEggs) / 2} eggs each!");
 
-            Console.WriteLine($"The winner is: {winner}");
-            Console.WriteLine($"{winner} has found {winnersEggs} eggs!");
+            }
+            else if (ilkoEggs == sofiEggs)
+            {
+                Console.WriteLine($"Ilko and Sofi have the same number of eggs, and both are winners with {(eggs - kociEggs) / 2} eggs each!");
+
+            }
+            else if (sofiEggs == kociEggs)
+            {
+                Console.WriteLine($"Sofi and Koci have the same number of eggs, and both are winners with {(eggs - ilkoEggs) / 2} eggs each!");
+
+            }
+
+
+            if (winnerAvailable)
+            {
+                Console.WriteLine($"The winner is: {winner}");
+                Console.WriteLine($"{winner} has found {winnersEggs} eggs!");
+            }
         }
     }
 }
