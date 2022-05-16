273a7005584f48abbba282208d4bbf543e1289c8
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-16 23:08:31 +0300

    Stacks and Queues - 02. Basic Queue Operations

---
+++ Stacks and Queues/BasicQueueOperations/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int N = nums[0];
+            int S = nums[1];
+            int X = nums[2];
+
+            int[] queNums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                queue.Enqueue(queNums[i]);
+            }
+
+            for (int i = 0; i < S; i++)
+            {
+                if (queue.Count > 0)
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                if (queue.Contains(X))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    List<int> sortedQue = queue.OrderBy(x => x).ToList();
+                    Console.WriteLine($"{sortedQue[0]}");
+                }
+            }
+            
+        }
+    }
+}
