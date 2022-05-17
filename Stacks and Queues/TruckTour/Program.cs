776f4624e757273a398708508c6ad5a4e8d9dccc
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-17 23:21:51 +0300

    Stacks and Queues - 07. Truck Tour

---
+++ Stacks and Queues/TruckTour/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TruckTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<long> stations = new Queue<long>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] currentStation = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int availableTravelDistanceForTheCurrentStation = currentStation[0] - currentStation[1];
+                stations.Enqueue(availableTravelDistanceForTheCurrentStation);
+            }
+
+            if (stations.Sum() < 0)
+            {
+                return;
+            }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                bool hasTravelledSuccessfully = true;
+                long totalDistance = 0;
+
+                foreach (var item in stations)
+                {
+                    totalDistance += item;
+                    if (totalDistance < 0)
+                    {
+                        hasTravelledSuccessfully = false;
+                        break;
+                    }
+                }
+
+                if (hasTravelledSuccessfully)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                else
+                {
+                    stations.Enqueue(stations.Dequeue());
+                }
+
+            }
+
+
+        }
+    }
+}
