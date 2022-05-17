3370ff3090acd10067e3a145c09ea860fa587a14
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-18 00:16:30 +0300

    Stacks and Queues - 06. Songs Queue

---
+++ Stacks and Queues/SongsQueue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songs = new Queue<string>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries));
+
+            while (songs.Count > 0)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Play")
+                {
+                    songs.Dequeue();
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(String.Join(", ", songs));
+                }
+                else if (command.StartsWith("Add"))
+                {
+                    string newSongName = command.Substring(4);
+                    if (songs.Contains(newSongName))
+                    {
+                        Console.WriteLine($"{newSongName} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(newSongName);
+                    }
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
