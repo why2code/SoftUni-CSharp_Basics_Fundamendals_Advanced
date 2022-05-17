7276bf5579207d5c848619c1e108fd2eecd6d0c4
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-18 00:03:30 +0300

    Stacks and Queues - 05. Fashion Boutique

---
+++ Stacks and Queues/FashionBoutique/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            Stack<int> clothes = new Stack<int>(input);
+            int capacity = int.Parse(Console.ReadLine());
+            
+            int addedClothesValue = 0;
+            int usedRacksCount = 0;
+            while (clothes.Count > 0)
+            {
+                addedClothesValue += clothes.Peek();
+                if (addedClothesValue >= capacity)
+                {
+                    if (addedClothesValue == capacity)
+                    {
+                        clothes.Pop();
+                    }
+                    usedRacksCount++;
+                    addedClothesValue = 0;
+                }
+                else
+                {
+                    clothes.Pop();
+                }
+
+            }
+
+            //last check if any remaining clothes were left from the pile left after the last rack was filled
+            if (addedClothesValue > 0)
+            {
+                usedRacksCount++;
+            }
+            Console.WriteLine(usedRacksCount);
+        }
+    }
+}
