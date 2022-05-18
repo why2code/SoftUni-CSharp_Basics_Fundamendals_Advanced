0328440be4ae2723b426a3e066b6dad343e3bcb2
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-18 23:31:28 +0300

    Multidimensional Arrays - 01. Diagonal Difference

---
+++ MultidimensionalArrays/DiagonalDifference/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = values[j];
+                }
+            }
+
+            int diagonalOne = 0;
+            int diagonalTwo = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                diagonalOne += matrix[i,i];
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                diagonalTwo += matrix[j, n - j - 1];
+            }
+
+            Console.WriteLine($"{Math.Abs(diagonalOne - diagonalTwo):F0}");
+        }
+    }
+}
