80880586d63c2f2b18ce5eead0a1ebf6cf285157
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-18 15:14:52 +0300

    Stacks and Queues - 08. Balanced Parentheses

---
+++ Stacks and Queues/BalancedParenthesis/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace BalancedParenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] paranthes = Console.ReadLine().ToCharArray();
+            bool isBalanced = true;
+
+            if (paranthes.Length == 1)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < paranthes.Length; i++)
+                {
+                    if (isBalanced == false)
+                    {
+                        break;
+                    }
+
+                    for (int j = paranthes.Length - 1 - i; j >= i; j--)
+                    {
+                        //{ [ ( ) ] }
+                        //0 1 2 3 4 5
+                        if ((paranthes[i] == '[' || paranthes[i] == '{')
+                            && paranthes[i] == (char)(paranthes[j] - 2))
+                        {
+                            break;
+                        }
+                        else if (paranthes[i] == '('
+                            && paranthes[i] == (char)(paranthes[j] - 1))
+                        {
+                            break;
+                        }
+                        else if (paranthes[i] == paranthes[j]
+                            && char.Equals(' ', paranthes[i])) // for space, ensuring no other chars besides space and (,[,{,},],)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            isBalanced = false;
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
