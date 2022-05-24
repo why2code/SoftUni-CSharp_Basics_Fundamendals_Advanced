f08ec25b1ac494861b12bfa465f1047d0ef5541a
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-24 18:32:51 +0300

    SetsAndDictionariesAdvanced - 02. Set of Elements

--- SetsAndDictionariesAdvanced/SetsOfElements/Program.cs
+++ SetsAndDictionariesAdvanced/SetsOfElements/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SetsOfElements
 {
@@ -7,6 +9,39 @@ namespace SetsOfElements
         static void Main(string[] args)
         {
 
+            int[] nm = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int n = nm[0];
+            int m = nm[1];
+            
+            HashSet<int> numsOne = new HashSet<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numsOne.Add(number);
+            }
+
+            HashSet<int> numsTwo = new HashSet<int>();
+            for (int i = 0; i < m; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numsTwo.Add(number);
+            }
+
+
+            List<int> matchingNumbers = new List<int>();
+            foreach (var num in numsOne)
+            {
+                if (numsTwo.Contains(num))
+                {
+                    matchingNumbers.Add(num);
+                }
+            }
+
+            foreach (var num in matchingNumbers)
+            {
+                Console.Write(num + " ");
+            }
         }
     }
 }
