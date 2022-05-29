841b364852d14a7f128e6e8c4ddd7962e2dfacfe
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-29 18:11:30 +0300

    Streams_Files_And_Directories - Encrypt Decrypt File - BasicXor

---
+++ Streams_Files_And_Directories/Encrypt_DecryptFile_BasicXor/Encrypt_DecryptFile_BasicXor.cs
@@ -0,0 +1,89 @@
+﻿namespace Encrypt_DecryptFile_BasicXor
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"../../../../Resources/copyMe.png";
+            string outputFilePath = @"../../../Encrypted-copy.png";
+            string outputFileDecrypted = @"../../../Decrypted-copy.png";
+
+            EncryptFile(inputFilePath, outputFilePath);
+            DecryptFile(outputFilePath, outputFileDecrypted);
+
+        }
+
+        public static void EncryptFile(string inputFilePath, string outputFilePath)
+        {
+
+            using (var input = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (var output = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    var buff = new Byte[1024];
+                    while (true)
+                    {
+
+                        var dataRead = input.Read(buff, 0, buff.Length);
+                        if (dataRead == 0)
+                            break;
+
+                        const byte keyForEncryption = 135;
+
+                        //Encrypting
+                        for (int i = 0; i < dataRead; i++)
+                            buff[i] = (byte)(buff[i] ^ keyForEncryption);
+                        output.Write(buff, 0, dataRead);
+
+
+                    }
+
+                }
+
+            }
+                               
+
+        }
+
+
+        public static void DecryptFile(string outputFilePath, string outputFileDecrypted)
+        {
+
+            using (var input = new FileStream(outputFilePath, FileMode.Open))
+            {
+                using (var output = new FileStream(outputFileDecrypted, FileMode.Create))
+                {
+                    var buff = new Byte[1024];
+                    while (true)
+                    {
+
+                        var dataRead = input.Read(buff, 0, buff.Length);
+                        if (dataRead == 0)
+                            break;
+
+                        const byte keyForEncryption = 135;
+
+                        //Decrypting
+                        for (int i = 0; i < dataRead; i++)
+                            buff[i] = (byte)(buff[i] ^ keyForEncryption);
+                        output.Write(buff, 0, dataRead);
+
+
+                    }
+
+                }
+
+            }
+
+
+        }
+
+
+
+
+    }
+}
+
