a9879d30b9cb6bb4a202410c1febf4e8ceac9922
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-29 17:22:32 +0300

    Streams_Files_And_Directories - 03.Copy Binary File

---
+++ Streams_Files_And_Directories/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,37 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"../../../../Resources/copyMe.png";
+            string outputFilePath = @"../../../copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (var input = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (var output = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    var buff = new Byte[1024];
+                    while (true)
+                    {
+
+                        var dataRead = input.Read(buff, 0, buff.Length);
+                        if (dataRead == 0)
+                            break;
+                        output.Write(buff, 0, dataRead);
+                    }
+
+                }
+
+            }
+        }
+    }
+}
