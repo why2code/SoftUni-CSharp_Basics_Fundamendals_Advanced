9a896562719803e1fcf568f9e9df184bcd0a6b9b
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-29 20:58:31 +0300

    Streams_Files_And_Directories - 05. Copy Directory

---
+++ Streams_Files_And_Directories/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,32 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+                Directory.Delete(outputPath);
+
+            Directory.CreateDirectory(outputPath);
+            string[] filesList = Directory.GetFiles(inputPath);
+            foreach (var file in filesList)
+            {
+                string name = Path.GetFileName(file);
+                string dirToCopyInto = Path.Combine(outputPath, name);
+                File.Copy(name, dirToCopyInto);
+            }
+
+        }
+    }
+}
