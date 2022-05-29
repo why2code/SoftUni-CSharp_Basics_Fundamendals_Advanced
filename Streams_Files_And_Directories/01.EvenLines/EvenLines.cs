601b53c3e2c8fc5771b0e7f9e4c69e328ee6179a
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-29 16:44:48 +0300

    Streams, Files and Directories - 01.Even Lines and 02. Line Numbers

---
+++ Streams_Files_And_Directories/01.EvenLines/EvenLines.cs
@@ -0,0 +1,72 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"../../../../Resources/text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            var readStr = new StreamReader(inputFilePath);
+            var singleText = new StringBuilder();
+            var finalText = new StringBuilder();
+
+            string lineText = readStr.ReadLine();
+            int counter = 0;
+
+            using (readStr)
+            {
+                while (lineText != null)
+                {
+                   
+                    if (counter == 0 || counter % 2 == 0)
+                    {
+                        foreach (var ch in lineText)
+                        {
+                            if (ch.ToString() == "-"
+                                || ch.ToString() == ","
+                                || ch.ToString() == "."
+                                || ch.ToString() == "!"
+                                || ch.ToString() == "?")
+                            {
+                                singleText.Append('@');
+                                //lineText = lineText.Replace(ch, '@');
+                            }
+                            else
+                            {
+                                singleText.Append(ch);
+                            }
+                           
+                        }
+
+                        lineText = singleText.ToString();
+                        lineText = string.Join(" ", lineText.Split().Reverse());
+                        finalText.Append(lineText);
+                        finalText.AppendLine();
+
+                        //lineText = string.Join(" ", lineText.Split().Reverse());
+                        //lineText = lineText + Environment.NewLine;
+                    }
+
+                    counter++;
+                    singleText.Clear();
+                    lineText = readStr.ReadLine();
+
+                }
+
+                return finalText.ToString();
+
+            }
+        }
+    }
+}
---
+++ Streams_Files_And_Directories/LineNumbers/LineNumbers.cs
@@ -0,0 +1,45 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = "../../../../Resources/text.txt";
+            string outputFilePath = @"../../../output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] inputs = File.ReadAllLines(inputFilePath);
+            int lettersCounter = 0;
+            int punctioationMarksCounter = 0;
+            string[] output = new string[inputs.Length];
+
+            for (int i = 1; i <= inputs.Length; i++)
+            {
+                foreach (var ch in inputs[i - 1])
+                {
+                    if (char.IsLetter(ch))
+                    {
+                        lettersCounter++;
+                    }
+                    else if (char.IsPunctuation(ch))
+                    {
+                        punctioationMarksCounter++;
+                    }
+                }
+                output[i - 1] = ($"Line {i}: " + inputs[i - 1] + $" ({lettersCounter})({punctioationMarksCounter})").ToString();
+                lettersCounter = 0;
+                punctioationMarksCounter = 0;
+            }
+
+            File.WriteAllLines(outputFilePath, output);
+
+        }
+    }
+}
