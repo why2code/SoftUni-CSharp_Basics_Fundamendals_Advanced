2555a08b0ea6f2391a0dbf3bcf91e646814d2f6e
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-30 22:26:30 +0300

    FunctionalProgramming - 03_CustomMinFunction

---
+++ FunctionalProgramming/03_CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_CustomMinFunction
+{
+    internal class CustomMinFunction
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            Func<int[], int> minNumber = x => x.Min();
+            Console.WriteLine(minNumber(input));
+        }
+    }
+}
