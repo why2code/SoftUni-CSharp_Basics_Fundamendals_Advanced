f8ebb699fbfbcf995fcd5af7e8012b6256678b6e
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-31 00:07:54 +0300

    FunctionalProgramming - 04.FindEvensOrOdds

---
+++ FunctionalProgramming/04.FindEvensOrOdds/FindEvensOrOdds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.FindEvensOrOdds
+{
+    internal class FindEvensOrOdds
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            string typeOfNumbers = Console.ReadLine();
+            int startRange = range[0];
+            int endRange = range[1];
+
+            Predicate<int> isOdd = x => x % 2 != 0;
+            Predicate<int> isEven = x => x % 2 == 0;
+
+            for (int i = startRange; i <= endRange; i++)
+            {
+                if (typeOfNumbers == "odd")
+                {
+                    if (isOdd(i))
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+                else if (typeOfNumbers == "even")
+                {
+                    if (isEven(i))
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+            }
+
+        }
+    }
+}
