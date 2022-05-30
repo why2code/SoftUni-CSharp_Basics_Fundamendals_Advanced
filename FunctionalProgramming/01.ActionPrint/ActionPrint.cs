8fb1b40a775a2d4ec89e2ab7ec4e45efe604beeb
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-30 22:07:44 +0300

    Functional Programming - 01.ActionPrint

---
+++ FunctionalProgramming/01.ActionPrint/ActionPrint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ActionPrint
+{
+    internal class ActionPrint
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Action<string> printer = x => PrintArray(x);
+            printer(input);
+        }
+
+        private static void PrintArray(string input)
+        {
+            string[] names = input.Split(' ');
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
