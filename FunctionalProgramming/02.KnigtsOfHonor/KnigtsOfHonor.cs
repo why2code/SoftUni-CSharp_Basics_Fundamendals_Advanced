648206e63cf8fdf1025995b3e83ee286559d169c
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-30 22:17:38 +0300

    FunctionalProgramming - 02.KnigtsOfHonor

---
+++ FunctionalProgramming/02.KnigtsOfHonor/KnigtsOfHonor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02.KnigtsOfHonor
+{
+    internal class KnigtsOfHonor
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Action<string> printer = x => PrintAmendedNames(x);
+            printer(input);
+        }
+
+        private static void PrintAmendedNames(string input)
+        {
+            string[] names = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Sir {name}");
+            }
+        }
+    }
+}
