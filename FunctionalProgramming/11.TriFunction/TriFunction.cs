5da99d15386cb6c5419c805d90235c2428add18e
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-06-01 01:05:31 +0300

    FunctionalProgramming - 11.TriFunction

---
+++ FunctionalProgramming/11.TriFunction/TriFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.TriFunction
+{
+    internal class TriFunction
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Console.WriteLine(names.First(x => x.Select(symbol => (int) symbol).Sum() >= number));
+        }
+    }
+}
