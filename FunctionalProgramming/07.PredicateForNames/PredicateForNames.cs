9803103d85f5f1b8b7a9492bffc5cb801d5ab2bb
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-31 19:01:21 +0300

    FunctionalProgramming - 07.PredicateForNames

---
+++ FunctionalProgramming/07.PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    internal class PredicateForNames
+    {
+        static void Main(string[] args)
+        {
+            int lenghtOfNames = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Predicate<string> matchingName = name => name.Length <= lenghtOfNames;
+            foreach (var name in names)
+            {
+                if (matchingName(name))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
