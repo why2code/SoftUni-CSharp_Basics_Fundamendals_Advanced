21ea2acc9012a98298e86aaf3927d140d5e40c8c
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-05-31 18:08:42 +0300

    FunctionalProgramming - 06.ReverseAndExclude

---
+++ FunctionalProgramming/06.ReverseAndExclude/ReverseAndExclude.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    internal class ReverseAndExclude
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int specialDivider = int.Parse(Console.ReadLine());
+
+            Func<int[], int, int[]> clensedArray = (nums,specialNum) =>
+            {
+                int[] temp = nums.Where(x => x % specialNum != 0).ToArray();
+                return nums = temp.Reverse().ToArray();
+            };
+
+            inputNumbers = clensedArray(inputNumbers, specialDivider);
+            foreach (var numClensed in inputNumbers)
+            {
+                Console.Write($"{numClensed} ");
+            }
+
+        }
+    }
+}
