70d95b844edd8dd87a44e2ced6f989948ea92344
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-06-02 21:13:38 +0300

    DefiningClasses - 04. Opinion poll

--- DefiningClasses/DefiningClasses/StartUp .cs
+++ DefiningClasses/DefiningClasses/StartUp .cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DefiningClasses
 {
@@ -8,21 +9,41 @@ namespace DefiningClasses
         static void Main(string[] args)
         {
             int num = int.Parse(Console.ReadLine());
-            Family familia = new Family();
-
+            List<Person> peopleOver30 = new List<Person>();
             for (int i = 0; i < num; i++)
             {
                 string[] input = Console.ReadLine().Split();
                 string name = input[0];
                 int age = int.Parse(input[1]);
 
-                Person memberToAddIntoFamily = new Person(name, age);
-                familia.AddMember(memberToAddIntoFamily);
+                if (age > 30)
+                {
+                    peopleOver30.Add(new Person(name, age));
+                }
             }
 
-            //familia.GetOldestMember();
-            Person oldestPerson = familia.GetOldestMember();
-            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+            peopleOver30 = peopleOver30.OrderBy(x => x.Name).ToList();
+            foreach (var person in peopleOver30)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+
+            //int num = int.Parse(Console.ReadLine());
+            //Family familia = new Family();
+
+            //for (int i = 0; i < num; i++)
+            //{
+            //    string[] input = Console.ReadLine().Split();
+            //    string name = input[0];
+            //    int age = int.Parse(input[1]);
+
+            //    Person memberToAddIntoFamily = new Person(name, age);
+            //    familia.AddMember(memberToAddIntoFamily);
+            //}
+
+            ////familia.GetOldestMember();
+            //Person oldestPerson = familia.GetOldestMember();
+            //Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
 
         }
     }
