b178d5c5be9a1724df625c354cd967cb3f16c1ce
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-06-02 18:44:10 +0300

    Defining Classes - 01. Define a class Person

---
+++ DefiningClasses/DefiningClasses/StartUp .cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person("George", 18);
+
+        }
+    }
+}
