18a6b5d1ec25c0e4b7bc7784f53fd91a7c40a34e
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-06-08 21:51:42 +0300

    Generics - 01. Box of string

---
+++ Generics/Generics/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Box<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{value.GetType().FullName}: {value}";
+        }
+
+    }
+}
---
+++ Generics/Generics/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Generics
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //===============================================================================
+            //1. Generic Box of String
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var box = new Box<string>(input);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
