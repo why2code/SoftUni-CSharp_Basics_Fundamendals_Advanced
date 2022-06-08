36a037159e967e101daf3646d7336f26241b1912
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-06-08 22:06:11 +0300

    Generics - 02. Box of Integer

--- Generics/Generics/StartUp.cs
+++ Generics/Generics/StartUp.cs
@@ -8,14 +8,24 @@ namespace Generics
         static void Main(string[] args)
         {
             //===============================================================================
-            //1. Generic Box of String
+            //2. Generic Box of Integers
             int n = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
                 var input = Console.ReadLine();
-                var box = new Box<string>(input);
+                var box = new Box<int>(int.Parse(input));
                 Console.WriteLine(box.ToString());
             }
+
+            ////===============================================================================
+            ////1. Generic Box of String
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < n; i++)
+            //{
+            //    var input = Console.ReadLine();
+            //    var box = new Box<string>(input);
+            //    Console.WriteLine(box.ToString());
+            //}
         }
     }
 }
