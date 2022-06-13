0b03c8792efee827f22a914865ce52bf8291b703
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-06-13 22:27:34 +0300

    Interators and Comparators - uploading Problem Statements file and 01. ListyIterator

---
+++ IteratorsAndComparators/IteratorsAndComparators/ListyIterator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class ListyIterator
+    {
+        private List<string> elements;
+        private static int internalIndex = 0;
+        public List<string> Elements
+        {
+            get { return elements; }
+            private set { elements = value; }
+        }
+
+        public ListyIterator(params string[] elements)
+        {
+            this.Elements = elements.ToList();
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                internalIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.Elements.Count == 0)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(this.Elements[internalIndex]);
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (internalIndex + 1 < this.elements.Count)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ IteratorsAndComparators/IteratorsAndComparators/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            //first command guaranteed to be Create, so we generate the new ListyIterator
+            string[] commArgs = command.Split();
+            string[] filteredInputs = new string[commArgs.Length - 1];
+            for (int i = 1; i < commArgs.Length; i++)
+            {
+                filteredInputs[i - 1] = commArgs[i];
+            }
+
+            var listy = new ListyIterator(filteredInputs);
+
+
+            while (command != "END")
+            {
+                if (command == "Move")
+                {
+                    bool commandResult = listy.Move();
+                    Console.WriteLine(commandResult);
+                }
+                else if (command == "HasNext")
+                {
+                    bool commandResult = listy.HasNext();
+                    Console.WriteLine(commandResult);
+                }
+                else if (command == "Print")
+                {
+                    listy.Print();
+                }
+
+                command = Console.ReadLine();
+            }
+
+        }
+    }
+}
