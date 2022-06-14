9016801ecdf4ac9f1c020cfc4711a85e2a99f58a
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-06-14 22:29:31 +0300

    IteratorsAndComparators - 05.ComparingObjects

---
+++ IteratorsAndComparators/05.ComparingObjects/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _05.ComparingObjects
+{
+    public class Person :IComparable<Person>
+    {
+        private string name;
+
+        private int age;
+
+        private string town;
+
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+        public string Town
+        {
+            get { return town; }
+            set { town = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int CompareTo( Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            //if names match, we check Age
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+            //if Age also matches, finally we check for Town
+            if (result == 0)
+            {
+                result = this.Town.CompareTo(other.Town);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ IteratorsAndComparators/05.ComparingObjects/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            List<Person> people = new List<Person>();
+            while (command != "END")
+            {
+                string[] commArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = commArgs[0];
+                int age = int.Parse(commArgs[1]);
+                string town = commArgs[2];
+
+                people.Add(new Person(name, age, town));
+
+                command = Console.ReadLine();
+            }
+
+            int index = int.Parse(Console.ReadLine());
+            int totalPeopleCount = people.Count;
+            int equalPeople = 0;
+
+            foreach (var person in people)
+            {
+                if (people[index - 1].CompareTo(person) == 0) //Index - 1 as we count from 1. Check full matching of Name, Age and Town
+                {
+                    equalPeople++;
+                }
+            }
+
+            if (equalPeople <= 1) //this is because ComparedTo will always produce at least 1 match (1 person matching himself)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                int notEqualPeople = totalPeopleCount - equalPeople;
+                Console.WriteLine($"{equalPeople} {notEqualPeople} {totalPeopleCount}");
+            }
+
+        }
+    }
+}
