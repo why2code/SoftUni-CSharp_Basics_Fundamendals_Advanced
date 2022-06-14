9092640b6fee0619348a1b24686066064bbd6145
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-06-14 21:14:11 +0300

    IteratorsAndComparators - 03.Stack

---
+++ IteratorsAndComparators/03.Stack/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03.Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+
+        public Stack(params T[] data)
+        {
+            elements = new List<T>(data.ToList());
+        }
+
+        public void Push(params T[] data)
+        {
+            foreach (var item in data)
+            {
+                elements.Insert(elements.Count, item);
+            }
+        }
+
+        public T Pop()
+        {
+            if (elements.Count == 0)
+            {
+                throw new ArgumentException("No elements");
+            }
+
+            T currElement = elements[elements.Count - 1];
+            elements.RemoveAt(elements.Count - 1);
+            return currElement;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = elements.Count - 1; i >= 0; i--)
+            {
+                yield return elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ IteratorsAndComparators/03.Stack/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            var stack = new Stack<int>();
+            while (command != "END")
+            {
+                string[] commArgs = command.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (commArgs[0] == "Push")
+                {
+                    var valueToPush = commArgs.Skip(1).Select(int.Parse).ToArray();
+                    stack.Push(valueToPush);
+
+                }
+                else if (commArgs[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (Exception e)
+                    {
+                        //throw new ArgumentException(e.Message);
+                        Console.WriteLine("No elements");
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
