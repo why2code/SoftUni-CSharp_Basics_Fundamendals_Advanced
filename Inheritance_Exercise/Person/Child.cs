986bb7741acefffb2b537517f152c27561875b91
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-07-04 17:55:08 +0300

    Inheritance - Person and Child class

---
+++ Inheritance_Exercise/Person/Child.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Child : Person
+    {
+
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+
+        //Extending the logic of the base setter for Age
+        public override int Age
+        {
+            get => base.Age;
+            set
+            {
+                if (value > 15)
+                {
+                    base.Age = 15;
+                }
+                else
+                {
+                    base.Age = value;
+                }
+            }
+        }
+    }
+}
---
+++ Inheritance_Exercise/Person/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        private int age;
+
+        public virtual int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value < 0)
+                {
+                    this.age = 0;
+                    //throw new ArgumentException("No negative age can be set!");
+                }
+                else
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
