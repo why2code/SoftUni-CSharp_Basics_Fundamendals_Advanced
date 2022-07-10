62bb35953b751feb8d08802b79b247e985f5744f
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-07-10 22:45:19 +0300

    Encapsulation 03. ShoppingSpree

---
+++ Encapsulation/ShoppingSpree/Person.cs
@@ -0,0 +1,84 @@
+﻿namespace ShoppingSpree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private readonly ICollection<Product> products;
+
+        public Person()
+        {
+            this.products = new List<Product>();
+        }
+
+        public Person(string name, decimal money)
+            : this()
+        {
+            this.Name = name;
+            this.Money = money;
+        }
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+        public decimal Money
+        {
+            get { return money; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+        public IReadOnlyCollection<Product> Products
+         => (IReadOnlyCollection<Product>)this.products;
+
+        public void AddProduct(Product product)
+        {
+            this.products.Add(product);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (this.products.Count == 0)
+            {
+                sb.Append($"{this.Name} - Nothing bought");
+            }
+            else
+            {
+                int counter = 0;
+                sb.Append($"{this.Name} - ");
+                foreach (var product in this.products)
+                {
+                    counter++;
+                    if (counter < this.products.Count)
+                    {
+                        sb.Append($"{product.Name}, ");
+                    }
+                    else
+                    {
+                        sb.Append($"{product.Name}");
+                    }
+                }
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Encapsulation/ShoppingSpree/StartUp.cs
@@ -0,0 +1,82 @@
+﻿namespace ShoppingSpree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> buyersList = new List<Person>();
+            Dictionary<string, decimal> productsList = new Dictionary<string, decimal>();
+
+            try
+            {
+                //Creating the buyers list from input
+                string[] peopleAndCash = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < peopleAndCash.Length; i++)
+                {
+                    string personsName = peopleAndCash[i].Split('=')[0];
+                    decimal personsMoney = decimal.Parse(peopleAndCash[i].Split('=')[1]);
+                    
+                    Person currentPerson = new Person(personsName, personsMoney);
+                    buyersList.Add(currentPerson);
+                }
+
+                //Creating the products list from input
+                string[] productsAndPrice = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < productsAndPrice.Length; i++)
+                {
+                    string productName = productsAndPrice[i].Split('=')[0];
+                    decimal productCost = decimal.Parse(productsAndPrice[i].Split('=')[1]);
+
+                    //just ensuring the product ctor works
+                    //Product currentProduct = new Product(productName, productCost);
+                    productsList[productName] = productCost;
+                }
+
+                string command = Console.ReadLine();
+                while (command != "END")
+                {
+                    string nameOfBuyer = command.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0];
+                    string nameOfProduct = command.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1];
+
+                    //making sure such buyer exists in the buyersList
+                    if (buyersList.FirstOrDefault(x => x.Name == nameOfBuyer) != null)
+                    {
+                        Person currentBuyer = buyersList.FirstOrDefault(x => x.Name == nameOfBuyer);
+                        decimal priceRemainingForCurrentBuyer = currentBuyer.Money;
+                        decimal productPrice = productsList[nameOfProduct];
+
+                        if (productPrice <= priceRemainingForCurrentBuyer)
+                        {
+                            currentBuyer.AddProduct(new Product(nameOfProduct, productPrice));
+                            currentBuyer.Money -= productPrice;
+                            Console.WriteLine($"{currentBuyer.Name} bought {nameOfProduct}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{currentBuyer.Name} can't afford {nameOfProduct}");
+                        }
+                    }
+
+                    command = Console.ReadLine();
+                }
+
+                //printing final list of buyers and products
+                foreach (Person buyer in buyersList)
+                {
+                    Console.WriteLine(buyer.ToString());
+                }
+
+            }
+            catch (Exception ae)
+            {
+                Console.WriteLine((ae.Message));
+                return;
+            }
+          
+        }
+    }
+}
